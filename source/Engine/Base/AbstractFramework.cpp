5cd7359a36efcc1cd6e664e19973c0ab34f6c053
Leah Lindner <hello@leah-lindner.com>
2017-04-24 19:10:06 +0100

    force core profile

--- source/Engine/Base/AbstractFramework.cpp
+++ source/Engine/Base/AbstractFramework.cpp
@@ -69,6 +69,7 @@ void AbstractFramework::Run()
 void AbstractFramework::InitializeSDL()
 {
 	// Request an OpenGL 4.5 context (should be core)
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
 	SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1);
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 5);
