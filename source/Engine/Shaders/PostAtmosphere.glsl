22eff71681d3bdd0f81e24d07a73528836e91996
Robert Lindner <robert.lindner@student.howest.be>
2017-05-03 00:56:23 +0100

    fake atmospheric scattering

--- source/Engine/Shaders/PostAtmosphere.glsl
+++ source/Engine/Shaders/PostAtmosphere.glsl
@@ -34,7 +34,45 @@
 	//Light
 	uniform vec3 Position;
 	uniform float Radius;
+	uniform float SurfaceRadius;
 	
+	uniform float EPSILON = 0.0001f;
+	
+	bool intersectAtmosphere(in vec3 viewDir, inout float offset, inout float maxPathLength)
+	{
+		offset = 0.0f;
+		maxPathLength = 0.0f;
+		// vector from ray origin to center of the sphere
+		vec3 l = Position-camPos;
+		float l2 = dot(l,l);
+		// adjust top atmosphere boundary by small epsilon to prevent artifacts
+		float r = Radius;// - EPSILON;
+		float r2 = r*r;
+		float s = dot(l,viewDir);
+		if(l2 <= r2)
+		{
+			// ray origin inside sphere, hit is ensured
+			float m2 = l2 - (s * s);
+			float q = sqrt(r2 - m2);
+			maxPathLength = s + q;
+			return true;
+		}
+		else if(s >= 0)
+		{
+			// ray starts outside in front of sphere, hit is possible
+			float m2 = l2 - (s * s);
+			float q2 = r2 - m2;
+			if(q2 >= 0)
+			{
+				// ray hits atmosphere definitely
+				float q = sqrt(q2);
+				offset = s - q;
+				maxPathLength = (s + q) - offset;
+				return true;
+			}
+		}
+		return false;
+	}	
 	
 	void main()
 	{
@@ -45,9 +83,30 @@
 		UNPACK_GBUFFER(tc, viewRay) //maybe use ao??
 		
 		//View dir and reflection
-		vec3 viewDir = -normalize(viewRay);
+		vec3 viewDir = normalize(viewRay);
+		
+		//Calculate atmoSPHERE intersections
+		float offset;
+		float maxPathLength;
+		bool hit = intersectAtmosphere(viewDir, offset, maxPathLength);
+		
+		//calculate the position of the back of the atmosphere
+		float bfDistance = offset+maxPathLength;
+		float terrainDistance = length(pos-camPos);
+		if((hit) && (bfDistance < terrainDistance))
+		{
+			pos = camPos + viewDir * bfDistance;
+		}
+		else
+		{
+			maxPathLength = terrainDistance - offset;
+		}
+		
+		float opacity = maxPathLength/(Radius);
+		opacity = pow(opacity, 1);
+		opacity = opacity*100;
 		
 		//output
-		outColor = vec4(pos/Radius, 0.50);
+		outColor = vec4(vec3(0.5, 0.5, 1), hit?opacity:0);
 	}
 </FRAGMENT>
