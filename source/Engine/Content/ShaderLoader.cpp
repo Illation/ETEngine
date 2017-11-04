84fa6019a208053bf36e54420700f30bc7994c00
Robert Lindner <robert.lindner@student.howest.be>
2017-11-04 18:17:01 +0000

    Using filesystem for shader parsing

--- source/Engine/Content/ShaderLoader.cpp
+++ source/Engine/Content/ShaderLoader.cpp
@@ -2,7 +2,9 @@
 #include "ShaderLoader.hpp"
 
 #include "../Graphics/ShaderData.hpp"
+#include "FileSystem/Entry.h"
 #include <algorithm>
+#include "FileSystem/FileUtil.h"
 
 ShaderLoader::ShaderLoader()
 {
@@ -19,33 +21,111 @@ ShaderData* ShaderLoader::LoadContent(const std::string& assetFile)
 
 	cout << "Building Shader: " << assetFile << " . . . ";
 
-	string vertSource;
-	string geoSource;
-	string fragSource;
+	File* input = new File( assetFile, nullptr );
+	if(!input->Open( FILE_ACCESS_MODE::Read ))
+	{
+		cout << "  . . . FAILED!" << endl;
+		cout << "    Opening shader file failed." << endl;
+		return nullptr;
+	}
+	std::string shaderContent = input->Read();
+	delete input; 
+	input = nullptr;
+	if(shaderContent.size() == 0)
+	{
+		cout << "  . . . FAILED!" << endl;
+		cout << "    Shader file is empty." << endl;
+		return nullptr;
+	}
 
+	//Precompile
+	bool useGeo = false;
+	bool useFrag = false;
+	std::string vertSource;
+	std::string geoSource;
+	std::string fragSource;
+	if(!Precompile( shaderContent, assetFile, useGeo, useFrag, vertSource, geoSource, fragSource ))
+	{
+		return nullptr;
+	}
+
+	//Compile
+	GLuint vertexShader = CompileShader(vertSource, GL_VERTEX_SHADER);
+	GLuint geoShader = 0;
+	if(useGeo)geoShader = CompileShader(geoSource, GL_GEOMETRY_SHADER);
+	GLuint fragmentShader = 0;
+	if (useFrag) fragmentShader = CompileShader(fragSource, GL_FRAGMENT_SHADER);
+
+	//Combine Shaders
+	GLuint shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	if(useGeo)glAttachShader(shaderProgram, geoShader);
+	if (useFrag)glAttachShader(shaderProgram, fragmentShader);
+	glBindFragDataLocation(shaderProgram, 0, "outColor");
+	glLinkProgram(shaderProgram);
+
+	cout << "  . . . SUCCESS!" << endl;
+
+	ShaderData* pShaderData = nullptr;
+	if (useGeo) pShaderData = new ShaderData(shaderProgram, vertexShader, geoShader, fragmentShader);
+	else if (useFrag) pShaderData =  new ShaderData(shaderProgram, vertexShader, fragmentShader);
+	else pShaderData = new ShaderData(shaderProgram, vertexShader);
+	pShaderData->m_Name = assetFile;
+	
+	return pShaderData;
+}
+
+GLuint ShaderLoader::CompileShader(const std::string &shaderSourceStr, GLenum type)
+{
+	using namespace std;
+	const char *shaderSource = shaderSourceStr.c_str();
+	GLuint shader = glCreateShader(type);
+	glShaderSource(shader, 1, &shaderSource, NULL);
+
+	//error handling
+	GLint status;
+	glCompileShader(shader);
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
+	if (!(status == GL_TRUE))
+	{
+		char buffer[512];
+		glGetShaderInfoLog(shader, 512, NULL, buffer);
+		cout << "  . . . FAILED!" << endl;
+		string sName;
+		switch (type)
+		{
+		case GL_VERTEX_SHADER:
+			sName = "vertex";
+			break;
+		case GL_GEOMETRY_SHADER:
+			sName = "geometry";
+			break;
+		case GL_FRAGMENT_SHADER:
+			sName = "fragment";
+			break;
+		default:
+			sName = "invalid type";
+			break;
+		}
+		cout << "    Compiling "<< sName << " shader failed." << endl;
+		cout << "    " << buffer << endl;
+	}
+
+	return shader;
+}
+
+bool ShaderLoader::Precompile(std::string &shaderContent, const std::string &assetFile, bool &useGeo, bool &useFrag, std::string &vertSource, std::string &geoSource, std::string &fragSource)
+{
 	enum ParseState {
 		INIT,
 		VERT,
 		GEO,
 		FRAG
 	} state = ParseState::INIT;
-	bool useGeo = false;
-	bool useFrag = false;
 
-	string extractedLine;
-	ifstream shaderFile;
-	shaderFile.open(assetFile);
-	if (!shaderFile)
+	std::string extractedLine;
+	while(FileUtil::ParseLine( shaderContent, extractedLine ))
 	{
-		cout << "  . . . FAILED!" << endl;
-		cout << "    Opening shader file failed." << endl;
-		return nullptr;
-	}
-	while (shaderFile.eof() == false)
-	{
-		//Get the line
-		getline(shaderFile, extractedLine);
-
 		//Includes
 		if (extractedLine.find("#include") != std::string::npos)
 		{
@@ -53,7 +133,7 @@ ShaderData* ShaderLoader::LoadContent(const std::string& assetFile)
 			{
 				cout << "  . . . FAILED!" << endl;
 				cout << "    Opening shader file failed." << endl; 
-				return nullptr;
+				return false;
 			}
 		}
 
@@ -111,82 +191,20 @@ ShaderData* ShaderLoader::LoadContent(const std::string& assetFile)
 			break;
 		}
 	}
-	shaderFile.close();
 
-	//Compile
-	GLuint vertexShader = CompileShader(vertSource, GL_VERTEX_SHADER);
-	GLuint geoShader = 0;
-	if(useGeo)geoShader = CompileShader(geoSource, GL_GEOMETRY_SHADER);
-	GLuint fragmentShader = 0;
-	if (useFrag) fragmentShader = CompileShader(fragSource, GL_FRAGMENT_SHADER);
-
-	//Combine Shaders
-	GLuint shaderProgram = glCreateProgram();
-	glAttachShader(shaderProgram, vertexShader);
-	if(useGeo)glAttachShader(shaderProgram, geoShader);
-	if (useFrag)glAttachShader(shaderProgram, fragmentShader);
-	glBindFragDataLocation(shaderProgram, 0, "outColor");
-	glLinkProgram(shaderProgram);
-
-	cout << "  . . . SUCCESS!" << endl;
-
-	ShaderData* pShaderData = nullptr;
-	if (useGeo) pShaderData = new ShaderData(shaderProgram, vertexShader, geoShader, fragmentShader);
-	else if (useFrag) pShaderData =  new ShaderData(shaderProgram, vertexShader, fragmentShader);
-	else pShaderData = new ShaderData(shaderProgram, vertexShader);
-	pShaderData->m_Name = assetFile;
-	
-	return pShaderData;
-}
-
-GLuint ShaderLoader::CompileShader(const std::string &shaderSourceStr, GLenum type)
-{
-	using namespace std;
-	const char *shaderSource = shaderSourceStr.c_str();
-	GLuint shader = glCreateShader(type);
-	glShaderSource(shader, 1, &shaderSource, NULL);
-
-	GLint status;
-	glCompileShader(shader);
-	glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
-	if (!(status == GL_TRUE))
-	{
-		char buffer[512];
-		glGetShaderInfoLog(shader, 512, NULL, buffer);
-		cout << "  . . . FAILED!" << endl;
-		string sName;
-		switch (type)
-		{
-		case GL_VERTEX_SHADER:
-			sName = "vertex";
-			break;
-		case GL_GEOMETRY_SHADER:
-			sName = "geometry";
-			break;
-		case GL_FRAGMENT_SHADER:
-			sName = "fragment";
-			break;
-		default:
-			sName = "invalid type";
-			break;
-		}
-		cout << "    Compiling "<< sName << " shader failed." << endl;
-		cout << "    " << buffer << endl;
-	}
-
-	return shader;
+	return true;
 }
 
 bool ShaderLoader::ReplaceInclude(std::string &line, const std::string &assetFile)
 {
 	std::string basePath = "";
-	size_t lastFS = assetFile.rfind("/");
-	size_t lastBS = assetFile.rfind("\\");
-	size_t lastS = (size_t)(max((int32)lastFS, (int32)lastBS));
+	uint32 lastFS = assetFile.rfind("/");
+	uint32 lastBS = assetFile.rfind("\\");
+	uint32 lastS = static_cast<uint32>(max((int32)lastFS, (int32)lastBS));
 	if(!(lastS == std::string::npos))basePath = assetFile.substr(0, lastS) + "/";
 
-	size_t firstQ = line.find("\"");
-	size_t lastQ = line.rfind("\"");
+	uint32 firstQ = line.find("\"");
+	uint32 lastQ = line.rfind("\"");
 	if ((firstQ == std::string::npos) ||
 		(lastQ == std::string::npos) ||
 		lastQ <= firstQ)
@@ -194,26 +212,31 @@ bool ShaderLoader::ReplaceInclude(std::string &line, const std::string &assetFil
 		cout << "  . . . FAILED!" << endl;
 		cout << "    invalid include syntax." << endl;
 		cout << line << endl;
-		return nullptr;
+		return false;
 	}
 	firstQ++;
 	std::string path = basePath + line.substr(firstQ, lastQ - firstQ);
 
-	ifstream shaderFile;
-	shaderFile.open(path);
-	if (!shaderFile)
+	File* input = new File( path, nullptr );
+	if(!input->Open( FILE_ACCESS_MODE::Read ))
 	{
 		cout << "  . . . FAILED!" << endl;
-		cout << "    Opening shader file \"" << path << "\" failed." << endl;
-		return nullptr;
+		cout << "    Opening shader file failed." << endl;
+		return false;
 	}
-	std::string ret;
-	std::string extractedLine;
-	while (shaderFile.eof() == false)
+	std::string shaderContent = input->Read();
+	delete input;
+	input = nullptr;
+	if(shaderContent.size() == 0)
 	{
-		//Get the line
-		getline(shaderFile, extractedLine);
-
+		cout << "  . . . FAILED!" << endl;
+		cout << "    Shader file is empty." << endl;
+		return false;
+	}
+	line = "";
+	std::string extractedLine;
+	while(FileUtil::ParseLine( shaderContent, extractedLine ))
+	{
 		//Includes
 		if (extractedLine.find("#include") != std::string::npos)
 		{
@@ -221,15 +244,12 @@ bool ShaderLoader::ReplaceInclude(std::string &line, const std::string &assetFil
 			{
 				cout << "  . . . FAILED!" << endl;
 				cout << "    Opening shader file failed." << endl; 
-				return nullptr;
+				return false;
 			}
 		}
 		
-		ret += extractedLine;
-		ret += "\n";
+		line += extractedLine + "\n";
 	}
-	shaderFile.close();
-	line = ret;
 	return true;
 }
 
--- source/Engine/FileSystem/FileUtil.cpp
+++ source/Engine/FileSystem/FileUtil.cpp
@@ -3,34 +3,43 @@
 
 #include <limits>
 
-std::vector<std::string> FileUtil::ParseLines( std::string raw )
+static std::vector<std::string> newLineTokens{ "\r\n", "\n\r", "\n", "\r" };
+
+bool FileUtil::ParseLine( std::string &input, std::string &extractedLine )
 {
-	std::vector<std::string> result;
-	std::vector<std::string> tokens{ "\r\n", "\n\r", "\n", "\r" };
-	while(raw.size())
+	if(input.size() == 0)return false;
+
+	int32 closestIdx = std::numeric_limits<int32>::max();
+	uint32 tokenSize = 0;
+	for(auto token : newLineTokens)
 	{
-		int32 closestIdx = std::numeric_limits<int32>::max();
-		uint32 tokenSize = 0;
-		for(auto token : tokens)
+		int32 index = input.find( token );
+		if(index != std::string::npos && index < closestIdx)
 		{
-			int32 index = raw.find( token );
-			if(index != std::string::npos && index < closestIdx)
-			{
-				closestIdx = index;
-				tokenSize = token.size();
-			}
-		}
-		if(closestIdx != std::string::npos)
-		{
-			result.push_back( raw.substr( 0, closestIdx ) );
-			raw = raw.substr( closestIdx + tokenSize );
-			if(raw.size() == 0)result.push_back( raw );
-		}
-		else
-		{
-			result.push_back( raw );
-			raw = "";
+			closestIdx = index;
+			tokenSize = token.size();
 		}
 	}
-	return result;
+
+	if(closestIdx != std::string::npos && static_cast<uint32>(closestIdx) < input.size())
+	{
+		extractedLine = input.substr( 0, closestIdx );
+		input = input.substr( closestIdx + tokenSize );
+		if(input.size() == 0) input = "";
+		return true;
+	}
+	extractedLine = input;
+	input = "";
+	return true;
+}
+
+std::vector<std::string> FileUtil::ParseLines( std::string raw )
+{
+	std::vector<std::string> ret;
+	std::string extractedLine;
+	while(FileUtil::ParseLine(raw, extractedLine))
+	{
+		ret.push_back( extractedLine );
+	}
+	return ret;
 }
