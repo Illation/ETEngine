71b7dbb9f584877e61b2931890695cf428189859
Robert Lindner <robert.lindner@student.howest.be>
2017-11-05 23:58:39 +0000

    added timer abstraction from original planet renderer

--- source/Engine/Base/Time.cpp
+++ source/Engine/Base/Time.cpp
@@ -8,24 +8,23 @@ Time::Time()
 
 
 Time::~Time()
-{
-}
+{}
 
 void Time::Start()
 {
-	begin = std::chrono::high_resolution_clock::now();
+	begin = Now();
 	last = begin;
 }
 void Time::Update()
 {
-	auto end = std::chrono::high_resolution_clock::now();
-	m_DeltaTime = ((float)(std::chrono::duration_cast<std::chrono::nanoseconds>(end - last).count()))*1e-9f;
+	auto end = Now();
+	m_DeltaTime = HRTCast<float>( Diff( last, end ) );
 	last = end;
 }
 float Time::GetTime()
 {
-	auto end = std::chrono::high_resolution_clock::now();
-	return ((float)(std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count()))*1e-9f;
+	auto end = Now();
+	return HRTCast<float>( Diff( begin, end ) );
 }
 float Time::DeltaTime()
 {
@@ -35,3 +34,43 @@ float Time::FPS()
 {
 	return (1.f / m_DeltaTime);
 }
+
+
+//Platform abstraction
+#ifdef PLATFORM_Linux
+
+	HighResTime Time::Now()const
+	{
+		HighResTime ret;
+		clock_gettime( CLOCK_PROCESS_CPUTIME_ID, &ret );
+		return ret;
+	}
+	HighResDuration Time::Diff( const HighResTime &start, const HighResTime &end )const
+	{
+		HighResDuration ret;
+		auto nsecDiff = end.tv_nsec - start.tv_nsec;
+		if(nsecDiff < 0)
+		{
+			ret.tv_sec = end.tv_sec - start.tv_sec - 1;
+			ret.tv_nsec = 1000000000 + nsecDiff;
+		}
+		else
+		{
+			ret.tv_sec = end.tv_sec - start.tv_sec;
+			ret.tv_nsec = nsecDiff;
+		}
+		return ret;
+	}
+
+#else
+
+	HighResTime Time::Now()const
+	{
+		return std::chrono::high_resolution_clock::now();
+	}
+	HighResDuration Time::Diff( const HighResTime &start, const HighResTime &end )const
+	{
+		return end - start;
+	}
+
+#endif
--- source/Engine/Base/Time.hpp
+++ source/Engine/Base/Time.hpp
@@ -1,5 +1,15 @@
 #pragma once
-#include <chrono>
+
+#ifdef PLATFORM_Linux
+	#include <time.h>
+	typedef timespec HighResTime;
+	typedef timespec HighResDuration;
+#else
+	#include <chrono>
+	typedef std::chrono::steady_clock::time_point HighResTime;
+	typedef std::chrono::duration<int64, std::nano> HighResDuration;
+#endif
+
 class Time
 {
 public:
@@ -12,8 +22,21 @@ public:
 	float FPS();
 
 private:
-	std::chrono::steady_clock::time_point begin;
-	std::chrono::steady_clock::time_point last;
+	//Platform abstraction
+	HighResTime Now()const;
+	HighResDuration Diff( const HighResTime &start, const HighResTime &end )const;
+
+	template<typename T>
+	T HRTCast( const HighResDuration &lhs )const
+	{
+	#ifdef PLATFORM_Linux
+		return (T)lhs.tv_sec + ((T)lhs.tv_nsec / 1000000000);
+	#else
+		return ((T)(std::chrono::duration_cast<std::chrono::nanoseconds>(lhs).count()))*1e-9f;
+	#endif
+	}
+
+	HighResTime begin;
+	HighResTime last;
 	float m_DeltaTime;
 };
-
