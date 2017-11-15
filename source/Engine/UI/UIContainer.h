f4aa75c9606f96ad0cf91ca28b07ae49c7fd9efa
Robert Lindner <contact@robert-lindner.com>
2017-11-15 19:07:52 +0000

    ui system work

--- source/Engine/UI/UIContainer.h
+++ source/Engine/UI/UIContainer.h
@@ -6,22 +6,55 @@ class UIComponent;
 class UIContainer
 {
 public:
-	virtual iRect CalculateDimensions() = 0;
+	//Should set the world position and return a rectangle in world position and its local scale
+	virtual iRect CalculateDimensions(const ivec2 &worldPos) = 0;
+
+	//offset to world position
+	void SetLocalPos( ivec2 pos ) { m_Rect.pos = pos; }
 
 protected:
 	iRect m_Rect;
+	ivec2 m_WorldPos;
 };
 
 class UIContentBox : public UIContainer
 {
-	UIContentBox(ivec2 size, ivec2 localPos, UIComponent* comp)
-		:m_Rect.size(size), m_Rect.pos(localPos), m_Content(comp)
-	{ }
+public:
+	UIContentBox(ivec2 size, ivec2 localPos, UIComponent* comp);
 
-	iRect CalculateDimensions() { return m_Rect; }
+	iRect CalculateDimensions( const ivec2 &worldPos);
 
 	void SetSize( ivec2 size ) { m_Rect.size = size; }
-	void SetLocalPos( ivec2 pos ) { m_Rect.pos = pos; }
+
+	UIComponent* GetContent() { return m_Content; }
 private:
 	UIComponent* m_Content;
+};
+
+class UIDynamicBox : public UIContainer
+{
+public:
+	enum class Mode : uint8
+	{
+		HORIZONTAL,
+		VERTICAL
+	};
+
+	enum class Positioning : uint8
+	{
+		DYNAMIC,	
+		UI_RELATIVE,	
+		SEPARATE
+	};
+
+	UIDynamicBox( UIDynamicBox::Mode mode ) :m_Mode( mode ) {}
+
+	iRect CalculateDimensions( const ivec2 &worldPos);
+	void AddChild( UIContainer* child, Positioning positioning );
+private:
+	UIDynamicBox::Mode m_Mode;
+
+	std::vector<UIContainer*> m_RelativeChildren;
+	std::vector<UIContainer*> m_DynamicChildren;
+	std::vector<UIContainer*> m_SeparateChildren;
 };
