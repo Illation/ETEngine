d83b5a4798ca5d6eca8fe9b77a25f6cc9ad1f3e9
Leah Lindner <hello@leah-lindner.com>
2017-12-08 18:31:40 +0000

    ui work

--- source/Engine/UI/UIContainer.cpp
+++ source/Engine/UI/UIContainer.cpp
@@ -3,21 +3,6 @@
 
 #include <limits>
 
-UIContentBox::UIContentBox( ivec2 size, ivec2 localPos, UIComponent* comp ) 
-	:m_Content( comp )
-{
-	m_Rect.size = size; 
-	m_Rect.pos = localPos;
-}
-
-iRect UIContentBox::CalculateDimensions( const ivec2 &worldPos )
-{
-	m_WorldPos = worldPos;
-	iRect ret = m_Rect;
-	ret.pos = ret.pos + m_WorldPos;
-	return ret;
-}
-
 iRect UIDynamicBox::CalculateDimensions( const ivec2 &worldPos )
 {
 	m_WorldPos = worldPos;
@@ -62,7 +47,38 @@ iRect UIDynamicBox::CalculateDimensions( const ivec2 &worldPos )
 		m_Rect.size.y = posOffset.y - worldPos.y;
 		break;
 	}
+
+	//Combine relative and dynamic for final size calculation
+	minCorner.x = min( minCorner.x, m_Rect.pos.x );
+	minCorner.y = min( minCorner.y, m_Rect.pos.y );
+	maxCorner.x = max( maxCorner.x, m_Rect.pos.x + m_Rect.size.x );
+	maxCorner.y = max( maxCorner.y, m_Rect.pos.y + m_Rect.size.y );
+	m_Rect.pos = minCorner;
+	m_Rect.size = maxCorner - minCorner;
+	ret.pos = m_Rect.pos + m_WorldPos;
+	ret.size = m_Rect.size;
+
 	return ret;
+}
+
+bool UIDynamicBox::Draw( uint16 level ) const
+{
+	level --;
+	if(level > 0)
+	{
+		bool ret = false;
+		for(auto child : m_RelativeChildren)
+		{
+			ret |= child->Draw( level );
+		}
+		for(auto child : m_DynamicChildren)
+		{
+			ret |= child->Draw( level );
+		}
+		return ret;
+	}
+	//Draw this box -> nothing to do	
+	return false;
 }
 
 void UIDynamicBox::AddChild( UIContainer* child, Positioning positioning )
@@ -74,9 +90,6 @@ void UIDynamicBox::AddChild( UIContainer* child, Positioning positioning )
 		break;
 	case Positioning::UI_RELATIVE:
 		m_RelativeChildren.push_back( child );
-		break;
-	case Positioning::SEPARATE:
-		m_SeparateChildren.push_back( child );
 		break;
 	}
 }
