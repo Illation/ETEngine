ea3bc97460d40b2dd68d8e91f119d5d7a135dd62
Robert Lindner <contact@robert-lindner.com>
2017-12-12 18:37:40 +0000

    support for 3D textures

--- source/Engine/Graphics/TextureData.hpp
+++ source/Engine/Graphics/TextureData.hpp
@@ -22,6 +22,7 @@ struct TextureParameters
 	int32 magFilter = GL_LINEAR;
 	int32 wrapS = GL_REPEAT;
 	int32 wrapT = GL_REPEAT;
+	int32 wrapR = GL_REPEAT;
 	vec4 borderColor = vec4(1);
 
 	bool genMipMaps = false;
@@ -33,8 +34,8 @@ struct TextureParameters
 class TextureData
 {
 public:
-	TextureData(GLuint handle, int32 width, int32 height);
-	TextureData(int32 width, int32 height, int32 internalFormat, GLenum format, GLenum type);
+	TextureData(GLuint handle, int32 width, int32 height, int32 depth = 1);
+	TextureData(int32 width, int32 height, int32 internalFormat, GLenum format, GLenum type, int32 depth = 1);
 	~TextureData();
 
 	GLuint GetHandle() { return m_Handle; }
@@ -42,6 +43,8 @@ public:
 
 	void Build(void* data = NULL);
 	void SetParameters( TextureParameters params );
+
+	GLenum GetTarget() { return m_Depth == 1 ? GL_TEXTURE_2D : GL_TEXTURE_3D; }
 
 	// returns true if regenerated 
 	// if its a framebuffer texture upscaling won't work properly 
@@ -53,6 +56,8 @@ private:
 
 	int32 m_Width;
 	int32 m_Height;
+
+	int32 m_Depth; // a (default) value of 1 implies a 2D texture
 
 	//Setup
 	int32 m_InternalFormat = GL_RGB;
