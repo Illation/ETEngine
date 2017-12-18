11ab0a796ba5a08bae6d8bbf0f6d9482c36b7a43
Robert Lindner <contact@robert-lindner.com>
2017-12-18 18:40:06 +0000

    Atmospher precompute work

--- source/Engine/Shaders/AtmoPreComp/ComputeTransmittance.glsl
+++ source/Engine/Shaders/AtmoPreComp/ComputeTransmittance.glsl
@@ -12,8 +12,39 @@
 
 	layout(location = 0) out vec3 transmittance;
 
+	uniform AtmosphereParameters uAtmosphere;
+
+	void GetRMuFromTransmittanceTextureUv(in AtmosphereParameters atmosphere, in vec2 uv, out float r, out float mu) 
+	{
+		//assert(uv.x >= 0.0 && uv.x <= 1.0);
+		//assert(uv.y >= 0.0 && uv.y <= 1.0);
+		float x_mu = GetUnitRangeFromTextureCoord(uv.x, uTexTransmittanceW);
+		float x_r = GetUnitRangeFromTextureCoord(uv.y, uTexTransmittanceH);
+		// Distance to top atmosphere boundary for a horizontal ray at ground level.
+		float H = sqrt(atmosphere.top_radius * atmosphere.top_radius - atmosphere.bottom_radius * atmosphere.bottom_radius);
+		// Distance to the horizon, from which we can compute r:
+		float rho = H * x_r;
+		r = sqrt(rho * rho + atmosphere.bottom_radius * atmosphere.bottom_radius);
+		// Distance to the top atmosphere boundary for the ray (r,mu), and its minimum
+		// and maximum values over all mu - obtained for (r,1) and (r,mu_horizon) -
+		// from which we can recover mu:
+		float d_min = atmosphere.top_radius - r;
+		float d_max = rho + H;
+		float d = d_min + x_mu * (d_max - d_min);
+		mu = d == 0.0 * 1.0 ? float(1.0) : (H * H - rho * rho - d * d) / (2.0 * r * d);
+		mu = ClampCosine(mu);
+	}
+	vec3 ComputeTransmittanceToTopAtmosphereBoundaryTexture( in AtmosphereParameters atmosphere, in vec2 gl_frag_coord) 
+	{
+		vec2 texTransmittanceSize = vec2(uTexTransmittanceW, uTexTransmittanceH);
+		float r;
+		float mu;
+		GetRMuFromTransmittanceTextureUv( atmosphere, gl_frag_coord / texTransmittanceSize, r, mu);
+		return ComputeTransmittanceToTopAtmosphereBoundary(atmosphere, r, mu);
+	}
+
     void main() 
 	{
-		transmittance = ComputeTransmittanceToTopAtmosphereBoundaryTexture( ATMOSPHERE, gl_FragCoord.xy);
+		transmittance = ComputeTransmittanceToTopAtmosphereBoundaryTexture( uAtmosphere, gl_FragCoord.xy);
     }
 </FRAGMENT>
