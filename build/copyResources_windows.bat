44b2e5c398897ec77e19be56010e373c47425c0b
Robert Lindner <contact@robert-lindner.com>
2018-01-03 19:07:52 +0000

    work on project generation

--- build/copyResources_windows.bat
+++ build/copyResources_windows.bat
@@ -1,12 +1,14 @@
 SET SOLUTION_DIR=%1
 SET OUT_DIR=%2
+IF [%3] == [] SET PLATFORM="x32"
+ELSE SET PLATFORM=%3
 
 ECHO ON
-ECHO Solution Directory: %SOLUTION_DIR% ; Output Directory: %OUT_DIR%
+ECHO Solution Directory: %SOLUTION_DIR% ; Output Directory: %OUT_DIR% ; Platform: %PLATFORM%
 
-xcopy "%SOLUTION_DIR%..\dependancies\x32\sdl2\SDL2.dll" "%OUT_DIR%" /y/D/d
-xcopy "%SOLUTION_DIR%..\dependancies\x32\freeImage\FreeImage.dll" "%OUT_DIR%" /y/D/d
-xcopy "%SOLUTION_DIR%\Engine\StaticDependancies\Assimp32.dll" "%OUT_DIR%" /y/D/d
+xcopy "%SOLUTION_DIR%..\dependancies\%PLATFORM%\sdl2\SDL2.dll" "%OUT_DIR%" /y/D/d
+xcopy "%SOLUTION_DIR%..\dependancies\%PLATFORM%\freeImage\FreeImage.dll" "%OUT_DIR%" /y/D/d
+xcopy "%SOLUTION_DIR%..\dependancies\%PLATFORM%\assimp\assimp.dll" "%OUT_DIR%" /y/D/d
 
 xcopy "%SOLUTION_DIR%\Engine\Resources" "%OUT_DIR%\Resources" /s/i/y/d
 xcopy "%SOLUTION_DIR%\Engine\Shaders" "%OUT_DIR%\Shaders" /s/i/y/d
