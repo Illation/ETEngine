ad42ae01c83754a01995e2a22ac30acd86bc21b3
Robert Lindner <robert.lindner@student.howest.be>
2018-01-13 19:21:02 +0000

    made JSON dom more accessible and tested it

--- source/Testing/Engine/FileSystem/JSONtest.cpp
+++ source/Testing/Engine/FileSystem/JSONtest.cpp
@@ -3,6 +3,7 @@
 #include "../../../Engine/FileSystem/Entry.h"
 #include "../../../Engine/FileSystem/FileUtil.h"
 #include "../../../Engine/FileSystem/JSONparser.h"
+#include "../../../Engine/Math/MathUtil.hpp"
 
 TEST_CASE("Parse", "[json]")
 {
@@ -14,4 +15,27 @@ TEST_CASE("Parse", "[json]")
 	jsonFile = nullptr;
 	JSONobject* root = parser.GetRoot();
 	REQUIRE_FALSE(root == nullptr);
+	JSONbool* boolvalue = (*root)["boolvalue"]->b();
+	REQUIRE_FALSE(boolvalue == nullptr);
+	REQUIRE(boolvalue->value == true);
+
+	REQUIRE((*root)["falsevalue"]->b()->value == false);
+
+	REQUIRE((*root)["nullvalue"]->GetType() == JSON_Null);
+
+	REQUIRE((*(*root)["menu"]->obj())["id"]->str()->value == "File");
+
+	JSONarray* jnumbers = (*(*root)["menu"]->obj())["num \"array"]->arr();
+	REQUIRE((*jnumbers)[0]->GetType() == JSON_Number);
+	std::vector<double> numbers = jnumbers->NumArr();
+	REQUIRE(etm::nearEquals(numbers[0], 10.0) == true);
+	REQUIRE(etm::nearEquals(numbers[1], 3.345) == true);
+	REQUIRE(etm::nearEquals(numbers[2], 2.0e4) == true);
+	REQUIRE(etm::nearEquals(numbers[3], 4.53e-2) == true);
+	REQUIRE(etm::nearEquals(numbers[4], 0.4e12) == true);
+	REQUIRE(etm::nearEquals(numbers[5], -12.43e+3) == true);
+
+	JSONarray* menu = (*(*(*root)["menu"]->obj())["popup"]->obj())["menuitem"]->arr();
+	REQUIRE((*menu)[0]->GetType() == JSON_Object);
+	REQUIRE((*(*menu)[0]->obj())["onclick"]->str()->value == "CreateNewDoc()");
 }
