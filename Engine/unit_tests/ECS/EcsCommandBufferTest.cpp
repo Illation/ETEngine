cac30118ff7e523982a215a6b3ebad862fd1956f
Leah Lindner <illeahtion@gmail.com>
2020-01-12 03:51:32 +0000

    ecs command buffer unit test covers all functionality

--- Engine/unit_tests/ECS/EcsCommandBufferTest.cpp
+++ Engine/unit_tests/ECS/EcsCommandBufferTest.cpp
@@ -47,13 +47,38 @@ TEST_CASE("command buffer reparent", "[ecs]")
 	REQUIRE(ecs.GetChildren(ent0)[0] == ent1);
 }
 
-TEST_CASE("command buffer add", "[ecs]")
+TEST_CASE("command buffer add / duplicate / remove", "[ecs]")
 {
 	fw::EcsController ecs;
 
 	// generate entities
 	fw::T_EntityId ent0 = ecs.AddEntity(TestCComponent(fw::INVALID_ENTITY_ID));
-	fw::T_EntityId ent1 = ecs.AddEntity(TestCComponent(fw::INVALID_ENTITY_ID));
+	REQUIRE(ent0 == 0u);
+
+	class TestDuplicateSystem final : public fw::System<TestDuplicateSystem, TestCView>
+	{
+	public:
+		TestDuplicateSystem() = default;
+		// base class constructors don't do anything so are not needed unless we declare dependencies or init lookup variables
+
+		void Process(fw::ComponentRange<TestCView>& range)
+		{
+			fw::EcsCommandBuffer& cb = GetCommandBuffer();
+
+			for (TestCView& view : range)
+			{
+				cb.DuplicateEntity(view.GetCurrentEntity());
+			}
+		}
+	};
+
+	ecs.RegisterSystem<TestDuplicateSystem>();
+
+	ecs.Process();
+
+	REQUIRE(ecs.GetEntityCount() == 2u);
+	REQUIRE(ecs.HasComponent<TestCComponent>(1u));
+	REQUIRE(ecs.GetComponent<TestCComponent>(1u).val == fw::INVALID_ENTITY_ID);
 
 
 	class TestAddSystem final : public fw::System<TestAddSystem, TestCWriteView>
@@ -75,6 +100,7 @@ TEST_CASE("command buffer add", "[ecs]")
 		}
 	};
 
+	ecs.UnregisterSystem<TestDuplicateSystem>();
 	ecs.RegisterSystem<TestAddSystem>();
 
 	ecs.Process();
@@ -88,12 +114,66 @@ TEST_CASE("command buffer add", "[ecs]")
 	REQUIRE(ecs.HasComponent<TestBComponent>(addEnt));
 	REQUIRE(ecs.HasComponent<TestCComponent>(addEnt));
 
-	fw::T_EntityId addEnt2 = ecs.GetComponent<TestCComponent>(ent1).val;
+	fw::T_EntityId addEnt2 = ecs.GetComponent<TestCComponent>(1u).val;
 	REQUIRE(addEnt2 != fw::INVALID_ENTITY_ID);
 
 	REQUIRE(ecs.HasComponent<TestAComponent>(addEnt2));
 	REQUIRE(ecs.HasComponent<TestBComponent>(addEnt2));
 	REQUIRE(ecs.HasComponent<TestCComponent>(addEnt2));
+
+	class TestRemoveASystem final : public fw::System<TestRemoveASystem, TestBCView>
+	{
+	public:
+		TestRemoveASystem() = default;
+		// base class constructors don't do anything so are not needed unless we declare dependencies or init lookup variables
+
+		void Process(fw::ComponentRange<TestBCView>& range)
+		{
+			fw::EcsCommandBuffer& cb = GetCommandBuffer();
+
+			for (TestBCView& view : range)
+			{
+				cb.RemoveComponents<TestAComponent>(view.GetCurrentEntity());
+			}
+		}
+	};
+
+	ecs.UnregisterSystem<TestAddSystem>();
+	ecs.RegisterSystem<TestRemoveASystem>();
+
+	ecs.Process();
+	
+	REQUIRE_FALSE(ecs.HasComponent<TestAComponent>(addEnt));
+	REQUIRE(ecs.HasComponent<TestBComponent>(addEnt));
+	REQUIRE(ecs.HasComponent<TestCComponent>(addEnt));
+
+	REQUIRE_FALSE(ecs.HasComponent<TestAComponent>(addEnt2));
+	REQUIRE(ecs.HasComponent<TestBComponent>(addEnt2));
+	REQUIRE(ecs.HasComponent<TestCComponent>(addEnt2));
+
+	class TestRemoveSystem final : public fw::System<TestRemoveSystem, TestBCView>
+	{
+	public:
+		TestRemoveSystem() = default;
+		// base class constructors don't do anything so are not needed unless we declare dependencies or init lookup variables
+
+		void Process(fw::ComponentRange<TestBCView>& range)
+		{
+			fw::EcsCommandBuffer& cb = GetCommandBuffer();
+
+			for (TestBCView& view : range)
+			{
+				cb.RemoveEntity(view.GetCurrentEntity());
+			}
+		}
+	};
+
+	ecs.UnregisterSystem<TestRemoveASystem>();
+	ecs.RegisterSystem<TestRemoveSystem>();
+
+	ecs.Process();
+
+	REQUIRE(ecs.GetEntityCount() == 2u);
 }
 
 
