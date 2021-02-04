8e0385ef40df0e1d6898dec2591123ba3ec24578
Leah Lindner <illeahtion@gmail.com>
2021-02-04 13:45:10 +0000

    convection kernels cmath include

---
+++ Engine/third_party/convectionKernels/convectionKernelsImpl.cpp
@@ -0,0 +1,16 @@
+#include "ConvectionKernels/ConvectionKernels_Config.h"
+
+#define CVTT_SINGLE_FILE_IMPL
+
+#include "ConvectionKernels/ConvectionKernels_API.cpp"
+#include "ConvectionKernels/ConvectionKernels_BC67.cpp"
+#include "ConvectionKernels/ConvectionKernels_BC6H_IO.cpp"
+#include "ConvectionKernels/ConvectionKernels_BCCommon.cpp"
+
+#include <cmath>
+#include "ConvectionKernels/ConvectionKernels_ETC.cpp"
+
+#include "ConvectionKernels/ConvectionKernels_IndexSelector.cpp"
+#include "ConvectionKernels/ConvectionKernels_S3TC.cpp"
+#include "ConvectionKernels/ConvectionKernels_Util.cpp"
+
