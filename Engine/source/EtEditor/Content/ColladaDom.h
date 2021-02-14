dd79d846e6695848202e3d59d333a4acae8073fb
Leah Lindner <illeahtion@gmail.com>
2021-02-14 11:33:17 +0000

    Asset info in collada dom

--- Engine/source/EtEditor/Content/ColladaDom.h
+++ Engine/source/EtEditor/Content/ColladaDom.h
@@ -4,8 +4,31 @@
 namespace et {
 namespace edit {
 
+//--------------------------
+//
+// not all information is parsed into the DOM, but instead should be parsed on demand depending on import settings
+//
 namespace dae { // COLLADA
 
+
+//--------------------------
+// Asset
+//
+// Global information for the collada document
+//
+struct Asset
+{
+	enum class E_Axis : uint8
+	{
+		X,
+		Y,
+		Z
+	};
+
+	XML::Element* m_Element = nullptr;
+	float m_UnitToMeter = 1.f;
+	E_Axis m_UpAxis = E_Axis::Y;
+};
 
 //--------------------------
 // Document
@@ -14,7 +37,8 @@ namespace dae { // COLLADA
 //
 struct Document
 {
-
+	std::string m_Version;
+	Asset m_Asset;
 };
 
 
